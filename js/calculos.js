95b66196ac636371fce12d5b770e3e3b51bd082e
JesusCanales <jesuscanales@outlook.com>
2019-11-29 03:06:23 -0500

    Ingreso de dos números

---
+++ js/calculos.js
@@ -0,0 +1,10 @@
+var valor01 = parseInt(window.prompt("Ingrese el primer número: "));
+var valor02 = parseInt(window.prompt("Ingrese el segundo número: "));
+var suma = valor01 + valor02;
+
+/* document.write(num1);
+document.write(num2); */
+
+document.getElementById("numUno").innerHTML = "El primer número es: " + valor01 ;
+document.getElementById("numDos").innerHTML ="El segundo número es: " + valor02 ;
+document.getElementById("suma").innerHTML = "El resultado de la suma es: " + suma;
